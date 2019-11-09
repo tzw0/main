114bdbc3b86737d64e18bc1267e0041e0d36a0a8
Notahotdog-Umar Moiz <umarmoiz@gmail.com>
2019-11-09 13:42:49 +0800

    added toStringSplitLogArray

--- src/main/java/gameassets/Log.java
+++ src/main/java/gameassets/Log.java
@@ -12,7 +12,7 @@ public class Log extends TaskList{
         logTasklist = new TaskList();
     }
 
-    //todo limit log printSize
+
 
     /**
      * Empties the LogList.
@@ -20,9 +20,26 @@ public class Log extends TaskList{
      */
     public static void clearLogList(Farmio farmio){
         if (!farmio.getFarmer().getLogTaskList().isEmpty()) {
-           farmio.getFarmer().getLogTaskList().deleteAll();
+            farmio.getFarmer().getLogTaskList().deleteAll();
         }
     }
+
+    //todo limit log printSize
+    public ArrayList<String> invalidLog(){
+        ArrayList<String> List = new ArrayList<String>();
+        List.add("----------------INVALID-LOG-------------------");
+        List.add("Please enter a number greater than 0 ");
+        return List;
+    }
+
+    public ArrayList<String> emptyLog(){
+        ArrayList<String> List = new ArrayList<String>();
+        List.add("----------------EMPTY-LOG-------------------");
+        List.add("Please populate the log");
+        return List;
+    }
+
+
 
     /**
      * Converts the logList to a readable format with index number to be printed.
@@ -31,6 +48,7 @@ public class Log extends TaskList{
     @Override
     public ArrayList<String> toStringArray() {
         ArrayList<String> list = new ArrayList<String>();
+        list.add("----------------------LOG-------------------");
         for (int i = 0; i < this.size(); i++) {
             Task task = this.get(i);
             String tasktype = task.getType().toString();
@@ -77,5 +95,74 @@ public class Log extends TaskList{
 
         return list;
     }
+    /**
+     * Converts the logList to a readable format with index number to be printed.
+     * @return String Array to be printed by the UI
+     */
+    public ArrayList<String> toStringSplitLogArray(int num) {
 
+        ArrayList<String> splitList = new ArrayList<String>();
+        int noEntries = this.size();
+        int noAvailablePages = (int) Math.ceil((double)noEntries / 15);
+        if(num <= 0 || num > noAvailablePages){
+            splitList.addAll(invalidLog());
+        } else if(noEntries == 0){
+            splitList.addAll(emptyLog());
+        }  else {
+            splitList.add("----------------------LOG-------------------");
+            int lowerBound = (num-1) * 15;
+            int upperBound;
+            int arbNum = num*15;
+            if(noEntries < arbNum){
+                upperBound = noEntries;
+            } else {
+                upperBound = arbNum;
+            }
+
+            for (int i = lowerBound; i < upperBound; i++) {
+                Task task = this.get(i);
+                String tasktype = task.getType().toString();
+                String taskCondition = task.getCondition().toString();
+                String taskAction = task.getAction().toString();
+
+                String output = "";
+                output +=  ((i + 1) + ". " + "Farmer");
+                if (tasktype.equals("IF")) {
+                    output += " checks  ";
+                    if (taskCondition.equals("hasSeeds")) {
+                        output += "if he has seeds in his assets";
+                    } else if (taskCondition.equals("hasWheat")) {
+                        output += "if there is any wheat which can be harvested";
+                    } else if (taskCondition.equals("hasGrain")) {
+                        output += "if there's any grain which he can sell";
+                    } else {
+                        output += "if " + taskCondition;
+                    }
+                    output += " then he";
+                }
+                if (taskAction.equals("buySeeds")) {
+                    output += " buys some seeds";
+                } else if (taskAction.equals("harvestWheat")) {
+                    output += " harvest the wheat";
+                }
+                else if (taskAction.equals("plantSeeds")) {
+                    output += " plants his seedling";
+                }
+                else if (taskAction.equals("sellGrain")) {
+                    output += " sells his grain";
+                }
+                else if (taskAction.equals("gotoMarket")) {
+                    output += " travels to the Market";
+                }
+                else if (taskAction.equals("gotoWheatFarm")) {
+                    output += " travels to the Wheat Farm";
+                }
+                else {
+                    output += "";
+                }
+                splitList.add(output);
+            }
+        }
+        return splitList;
+    }
 }
