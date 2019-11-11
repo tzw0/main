9fb1618d8adc84f671b5a76eb39aff0122901561
tzw0 <e0310410@u.nus.edu>
2019-11-11 16:25:05 +0800

    fixed stylecheck for test for action & sim

--- src/test/java/SimulationTest.java
+++ src/test/java/SimulationTest.java
@@ -6,39 +6,48 @@ import frontend.UiDummy;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SimulationTest {
     Farmio farmio;
 
-    public SimulationTest() throws FarmioFatalException{
+    /**
+     * Initialises the a Farmio object with dummy ui and real storage.
+     *
+     * @throws FarmioFatalException if storage file of the level is not found.
+     */
+    public SimulationTest() throws FarmioFatalException {
         farmio = new Farmio(true);
         farmio.getFrontend().setDummyUi();
         farmio.setLevel(new Level(farmio.getStorage().getLevel(1.1), "tester"));
     }
+
     @Test
     void singleFullScreenSimulationTest() throws FarmioFatalException {
         farmio.getFrontend().simulate();
         assertEquals("sleepclearshow", UiDummy.uiTestString);
     }
+
     @Test
     void multiFullScreenSimulationTest() throws FarmioFatalException {
         UiDummy.uiTestString = "";
         farmio.getFrontend().simulate("Test", 0, 3, true);
         assertEquals("sleepclearshow".repeat(4), UiDummy.uiTestString);
     }
+
     @Test
     void singleSimulationTest() throws FarmioFatalException {
         UiDummy.uiTestString = "";
         farmio.getFrontend().simulate("Test",0);
         assertEquals("sleepclearshow", UiDummy.uiTestString);
     }
+
     @Test
     void multiFrameSimulationTest() throws FarmioFatalException {
         UiDummy.uiTestString = "";
         farmio.getFrontend().simulate("Test", 0, 3, true);
         assertEquals("sleepclearshow".repeat(4), UiDummy.uiTestString);
     }
+
     @Test
     void multiFrameReverseSimulationTest() throws FarmioFatalException {
         UiDummy.uiTestString = "";
